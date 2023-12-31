4e2263137fc84b474599a69610783710639ee041
Ramsay Leung <ramsayleung@gmail.com>
2023-12-30 22:50:08 -0800

    Add ErrorPage to display user not exist error

---
+++ components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export function UserNotExist(){
+    return <div className='flex flex-col mt-8 justify-center items-center'>
+        <p className='p-4'>Your profile doesn't exist</p>
+        <Image src="/cat_not_work.gif"
+              width={480}
+              height={480}
+              alt="The kitty is visting your profile"
+        />
+    </div>
+}
