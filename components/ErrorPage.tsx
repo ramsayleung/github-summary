eaf9822d9116bbb9825bc8ff4c8483447cd37f24
Ramsay Leung <ramsayleung@gmail.com>
2024-01-01 13:05:46 -0800

    Replace single quote with &apos;

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 
 export function UserNotExist(){
-    return <div className='flex flex-col mt-8 justify-center items-center'>
-        <p className='p-4'>Your profile doesn't exist</p>
+    return <div className="flex flex-col mt-8 justify-center items-center">
+        <p className="p-4">Your profile doesn&apos;t exist</p>
         <Image src="/cat_not_work.gif"
               width={480}
               height={480}
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 
 export function LoadingPage(){
-    return <div className='flex flex-col mt-8 justify-center items-center'>
-        <p className='p-4'>Please wait, I'm visiting your Github profile</p>
+    return <div className="flex flex-col mt-8 justify-center items-center">
+        <p className="p-4">Please wait, I&apos;m visiting your Github profile</p>
         <Image src="/cat_waitting.gif"
               width={480}
               height={480}
