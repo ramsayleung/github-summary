ba8a2aa3b4cdd27e4bddc3df2ea25bdecd209eb0
Ramsay Leung <ramsayleung@gmail.com>
2024-01-01 11:02:24 -0800

    Extract Navbar as a separate component.

--- app/page.tsx
+++ app/page.tsx
@@ -430,40 +430,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="grid grid-cols-4 px-8 sm:px-16 py-2 border-b">
-        <a className="flex col-span-3 items-center gap-2" href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"/></svg>
-          <h1 className="font-bold text-xl">Github Summary</h1>
-        </a>
-        <nav
-          aria-label="Main"
-          data-orientation="horizontal"
-          dir="ltr"
-          className="relative z-10 flex max-w-max flex-1 items-center justify-center gap-2"
-        >
-          <div>
-            <ul
-              data-orientation="horizontal"
-              className="group flex-1 list-none items-center justify-center space-x-1 hidden sm:flex"
-              dir="ltr"
-            >
-              <li>
-                <a
-                  className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors"
-                  href="/"
-                  data-radix-collection-item=""
-                >
-                  
-                  <svg aria-hidden="true" className="octicon octicon-mark-github mx-2" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-</svg>
-                  Github
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="absolute left-0 top-full flex justify-center"></div>
-        </nav>
-      </div>
+      <Navbar/>
 
       <div className="flex flex-col p-24 items-center justify-center w-full h-full bg-white">
         <div>
